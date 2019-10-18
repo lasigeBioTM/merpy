90c521ca03c08e5921105f80b874c1e297d0abe7
Andre Lamurias <alamurias@lasige.di.fc.ul.pt>
2019-10-18 10:25:59 +0100

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="merpy",
-    version="0.3.0",
+    version="0.3.1",
     description="use MER inside python",
     long_description=long_description,
     long_description_content_type="text/markdown",
